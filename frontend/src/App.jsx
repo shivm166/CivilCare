f2cd87d2bdac29b93e9c2e17e6d6ca43b920e044
jainish4126 <pateljainish2021@gmail.com>
2025-11-03 17:04:47 +0530

    commit dhulo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div className="bg-red-400">App hello from jainish</div>;
+  return <div className="bg-red-400">App hello from dhruvlo </div>;
 };
 
 export default App;
