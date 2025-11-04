8588251f5d29e71d284d3c1156aa6fd834da8c9a
shivam gauswami <shivamgauswami86@gmail.com>
2025-11-04 15:22:40 +0530

    pages added

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Container from "./Container.jsx";
 import HomePage from "../../pages/home/HomePage";
+import Header from "../layout/header/Header.jsx";
+import Footer from "../layout/footer/Footer.jsx";
 
 const Layout = () => {
   return (
     <>
       <div>
+        <Header />
         <Container>
           <HomePage />
         </Container>
+        <Footer />
       </div>
     </>
   );
