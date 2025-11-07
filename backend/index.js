664391a5170d3d9ef2bbea553d921e4d875326bf
jainish4126 <pateljainish2021@gmail.com>
2025-11-07 17:41:00 +0530

    create request controller model

--- backend/index.js
+++ backend/index.js
@@ -6,28 +6,29 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
 import societyRouter from "./routes/society.route.js";
+import requestRouter from "./routes/request.route.js"; // ⬅️ ADD THIS LINE
 import attachSocietyContext from "./middlelware/attachSocietyContext.js";
 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
+
 connDB();
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend or Postman domain
-    credentials: true, // ✅ allows cookies
+    origin: "http://localhost:5173",
+    credentials: true,
   })
 );
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(attachSocietyContext);
 
 app.use("/api/user", userRouter);
 app.use("/api/society", societyRouter);
+app.use("/api/request", requestRouter); // ⬅️ ADD THIS LINE
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
