3fb9a731da089feedc759f2060cce36230dff8a9
Akhil-virja <virjaakhil555@gmail.com>
2025-11-18 15:59:04 +0530

    wrap with routes of superadminroutes

--- frontend/src/routes/SuperAdminRoutes.jsx
+++ frontend/src/routes/SuperAdminRoutes.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import SuperAdminLayout from "../components/layout/SuperAdminLayout/SuperAdminLayout"
 import SuperAdminDashboard from "../pages/dashboard/SuperAdmin/SuperAdminDashboard/SuperAdminDashboard"
 import SuperAdminSocieties from "../pages/dashboard/SuperAdmin/SuperAdminSocieties/SuperAdminSocieties"
@@ -6,14 +6,14 @@ import SuperAdminUsers from "../pages/dashboard/SuperAdmin/SuperAdminUsers/Super
 
 function SuperAdminRoutes() {
   return (
-    <Route>
+    <Routes>
         <Route path="/home" element={<Navigate to="/superadmin/dashboard" replace />} />
         <Route path="/superadmin" element={<SuperAdminLayout />}>
             <Route path="/superadmin/dashboard" element={<SuperAdminDashboard />} />
             <Route path="/superadmin/societies" element={<SuperAdminSocieties />} />
             <Route path="/superadmin/users" element={<SuperAdminUsers />} />
         </Route>
-    </Route>
+    </Routes>
   )
 }
 
