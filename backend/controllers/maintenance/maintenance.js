1c6e9ab02aa51a87134e36c3ad5245c2587748fa
shivam gauswami <shivamgauswami86@gmail.com>
2025-12-01 11:14:48 +0530

    maintanence crud operation build and test

--- backend/controllers/maintenance/maintenance.js
+++ backend/controllers/maintenance/maintenance.js
@@ -53,19 +53,72 @@ export const postMaintanenceRule = async (req, res) => {
   }
 };
 
-// export const getMaintanenceRules = async (req, res) => {
-//   try {
-//     const maintenanceRules = await MaintenanceRule.find({
-//       society: req.society._id,
-//     });
-//     return sendSuccessResponse(
-//       res,
-//       SUCCESS,
-//       { maintenanceRules },
-//       "Maintenance rules fetched successfully"
-//     );
-//   } catch (error) {
-//     console.error("Error fetching maintenance rules :", error);
-//     return sendErrorResponse(res, SERVER_ERROR, error, "Internal server error");
-//   }
-// };
+export const getMaintanenceRules = async (req, res) => {
+  try {
+    const maintenanceRules = await MaintenanceRule.find({
+      society: req.society._id,
+    });
+    return sendSuccessResponse(
+      res,
+      SUCCESS,
+      { maintenanceRules },
+      "Maintenance rules fetched successfully"
+    );
+  } catch (error) {
+    console.error("Error fetching maintenance rules :", error);
+    return sendErrorResponse(res, SERVER_ERROR, error, "Internal server error");
+  }
+};
+
+export const updateMaintenanceRule = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      bhkType,
+      amount,
+      dueDay,
+      gracePeriod,
+      penaltyType,
+      penaltyValue,
+      active,
+    } = req.body;
+    const updatedRule = await MaintenanceRule.findByIdAndUpdate(
+      id,
+      {
+        bhkType,
+        amount,
+        dueDay,
+        gracePeriod,
+        penaltyType,
+        penaltyValue,
+        active,
+      },
+      { new: true }
+    );
+    return sendSuccessResponse(
+      res,
+      SUCCESS,
+      { updatedRule },
+      "Maintenance rule updated successfully"
+    );
+  } catch (error) {
+    console.error("Error updating maintenance rule :", error);
+    return sendErrorResponse(res, SERVER_ERROR, error, "Internal server error");
+  }
+};
+
+export const deleteMaintenanceRule = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await MaintenanceRule.findByIdAndDelete(id);
+    return sendSuccessResponse(
+      res,
+      SUCCESS,
+      null,
+      "Maintenance rule deleted successfully"
+    );
+  } catch (error) {
+    console.error("Error deleting maintenance rule :", error);
+    return sendErrorResponse(res, SERVER_ERROR, error, "Internal server error");
+  }
+}
