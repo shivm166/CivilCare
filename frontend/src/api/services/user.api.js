96924d040dd1ab5973bfdfe76ec307c8ef38dbe4
shivam gauswami <shivamgauswami86@gmail.com>
2025-12-02 12:13:04 +0530

    maintenance controller tested

--- frontend/src/api/services/user.api.js
+++ frontend/src/api/services/user.api.js
@@ -3,7 +3,6 @@ import { axiosInstance } from "../axios";
 export const updateProfile = async (profileData) => {
   try {
     const response = await axiosInstance.put("/user/profile", profileData);
-    // FIX: Return the entire response.data object, which contains { success, data, meta }
     return response.data;
   } catch (error) {
     console.log("Error in updating profile", error);
